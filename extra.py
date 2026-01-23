7eb763f94f43efd944100c2f2a8f713705319350
Razan222825 <razan.a.mahgoub@gmail.com>
2026-01-23 08:55:08 +0800

    change again

--- extra.py
+++ extra.py
@@ -4,3 +4,9 @@ h = float(input("enter value of height:" ))
 area= 1/2*b*h
 print (f"the area is {area}cm")
 #area of square 
+def square_area(width, height):
+    area = width * height 
+    return area
+
+result= square_area(5,10)
+print (f"the area is {result}")
