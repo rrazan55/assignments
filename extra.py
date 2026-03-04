da45dd01f8fae616921e11cfaeb0d59a9ad30113
Razan222825 <razan.a.mahgoub@gmail.com>
2026-03-04 09:49:35 +0800

    Update extra.py

--- extra.py
+++ extra.py
@@ -3,10 +3,38 @@ b = float(input ("enter value of base:"))
 h = float(input("enter value of height:" ))
 area= 1/2*b*h
 print (f"the area is {area}cm")
-#area of square 
-def square_area(width, height):
-    area = width * height 
-    return area
 
-result= square_area(5,10)
-print (f"the area is {result}")
+#area= 1/2*b*h
+#print (f"the area is {area}cm")
+#area of square 
+#def square_area(width, height):
+   # area = width * height 
+    #return area
+
+#result= square_area(5,10)
+#print (f"the area is {result}")
+
+#function revision 
+#def happy_birthday(name, old):
+   # print(f"happy birthday to {name}")
+
+
+#coverter program 
+#weight
+#Distance
+#tempretaure
+weight = int(input("enter the weight u wanna convert"))
+unit = input("gram or kilo-gram")
+if unit= kilo-gram :
+    print (f"the weight is {weight*1000}kg")
+else:
+    print (f"the weight is {weight}g")
+def choose(option, unit):
+
+
+
+
+
+
+
+
