99814ecfd156632a119439de318a618fa5ad62c8
Razan222825 <razan.a.mahgoub@gmail.com>
2026-03-09 14:15:07 +0800

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,9 +1,9 @@
 #area of triangle 
-b = float(input ("enter value of base:"))
-h = float(input("enter value of height:" ))
-area= 1/2*b*h
-print (f"the area is {area}cm")
 
+
+
+#b = float(input ("enter value of base:"))
+#h = float(input("enter value of height:" ))
 #area= 1/2*b*h
 #print (f"the area is {area}cm")
 #area of square 
